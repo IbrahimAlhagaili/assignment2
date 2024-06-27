1cc83af0ebac2d40f69f3a5c63421bbd0b3aeb86
tileSetting <leiscribbles@gmail.com>
2024-06-26 22:54:30 -0600

    Reverting back next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+
 };
 
-module.exports = nextConfig;
-export default nextConfig;//Original
+
+export default nextConfig;
